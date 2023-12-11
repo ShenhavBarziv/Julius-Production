2aff3f2eea41e6017600b231e92460f5563480b4
Shenhav <shenhavbarziv@gmail.com>
2023-12-12 00:21:02 +0200

    Change the host in cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://www.shenhav.xyz",
+    origin: "julius-production-api.vercel.app",
     credentials: true,
   })
 );
