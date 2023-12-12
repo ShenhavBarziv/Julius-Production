250338037f249eb71ba26bff94a260c479b2720a
Shenhav <shenhavbarziv@gmail.com>
2023-12-12 11:20:24 +0200

    delete vercel.json again and add methods in cors

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "https://www.shenhav.xyz",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,})
 );
 require("dotenv").config();
