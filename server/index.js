324b520482509dc8eb42b91e3c55f223d3d4654e
Shenhav <shenhavbarziv@gmail.com>
2023-12-12 16:45:14 +0200

    get / hii

--- server/index.js
+++ server/index.js
@@ -40,17 +40,11 @@ function validateInput(user) {
   }
   return true;
 }
-
+app.get("/", (req,res) => {
+  res.send('hii');
+})
 app.post("/login", async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+
   const { email, password } = req.body;
   if (validateInput(req.body)) {
     const data = await Login(email, password);
